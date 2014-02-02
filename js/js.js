b0575b21adbf1c67550d0b8aa10106a549365640
Eklers <anete.pazusa@gmail.com>
2014-02-02 15:19:22 +0200

    Update
    - list creation
    - filter and search
    - design

--- js/js.js
+++ js/js.js
@@ -1,4 +1,33 @@
-function menuSlider() {
-	
+function filterUse() {
+	if(document.getElementById('filter-sub-menu-nav').style.display == 'block'){
+		document.getElementById('user-sub-menu-nav').style.display = 'block';
+		document.getElementById('filter-sub-menu-nav').style.display = 'none';
+		document.getElementById('search-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-icon-image').style.backgroundImage = "url('img/icon-filter.png')";
+		document.getElementById('search-icon-image').style.backgroundImage = "url('img/icon-search.png')";
+	}
+	else{
+		document.getElementById('filter-sub-menu-nav').style.display = 'block';
+		document.getElementById('user-sub-menu-nav').style.display = 'none';
+		document.getElementById('search-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-icon-image').style.backgroundImage = "url('img/icon-filter-red.png')";
+		document.getElementById('search-icon-image').style.backgroundImage = "url('img/icon-search.png')";
+	}
 }
 
+function searchUse() {
+	if(document.getElementById('search-sub-menu-nav').style.display == 'block'){
+		document.getElementById('user-sub-menu-nav').style.display = 'block';
+		document.getElementById('search-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-icon-image').style.backgroundImage = "url('img/icon-filter.png')";
+		document.getElementById('search-icon-image').style.backgroundImage = "url('img/icon-search.png')";
+	}
+	else{
+		document.getElementById('search-sub-menu-nav').style.display = 'block';
+		document.getElementById('user-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-sub-menu-nav').style.display = 'none';
+		document.getElementById('filter-icon-image').style.backgroundImage = "url('img/icon-filter.png')";
+		document.getElementById('search-icon-image').style.backgroundImage = "url('img/icon-search-red.png')";
+	}
+}
